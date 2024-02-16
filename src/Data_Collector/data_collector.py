0d5f09c869a99229e70a886184dd700f8785821a
Ohad Shpindel <ohad.shpindel@solaredge.com>
2024-02-16 20:59:48 +0200

    update async

--- src/Data_Collector/data_collector.py
+++ src/Data_Collector/data_collector.py
@@ -2,7 +2,7 @@ import os
 import logging
 import requests
 import json
-
+import asyncio
 from enum import Enum
 from flask import jsonify
 from flask_caching import Cache
@@ -25,19 +25,22 @@ class DataCollector():
         
         self.default_headers = {'accept': 'application/json'}
         
-        if "IP_SERVERS" in os.environ:
-            self.ip_servers = json.loads(os.getenv("IP_SERVERS"))
+        if "IP_APIS" in os.environ:
+            self.ip_apis = json.loads(os.getenv("IP_APIS"))
         else:
-            self.ip_servers = {
+            # set default IP APIs
+            self.ip_apis = {
                 "ip-api" : {"url": "http://ip-api.com/json", "resp_time": 1},
                 "bgview": {"url": "https://api.bgpview.io/ip", "resp_time": 2},
             }
 
         
 
-    def get_request_handler(self, req:str) -> (requests.Response | None):
+    async def get_request_handler(self, req:str) -> (requests.Response | None):
+        # handle the requests.get commands and returns the response
         try:
             response = requests.get(req, headers=self.default_headers)
+            await asyncio.sleep(1)
             response.raise_for_status()
             data = response.json()
             return data
@@ -59,8 +62,9 @@ class DataCollector():
         except Exception as err:
             logging.critical("Unknown Error:", err)
             raise
-    def get_ip_server_data(self, url, ip):
-        ip_server_data = self.get_request_handler(f'{url}/{ip}')
+
+    async def get_ip_server_data(self, url, ip):
+        ip_server_data = await self.get_request_handler(f'{url}/{ip}')
         logging.info("get_ip_server_data return: %s", ip_server_data)
         return ip_server_data
 
@@ -75,9 +79,9 @@ class DataCollector():
         ip_resp = {IP_RESP_FIELDS.METRICS.value: {}, IP_RESP_FIELDS.RAW_DATA.value: {}}
         failure = False
         total_time = 0
-        for ip_server_name, server_params in self.ip_servers.items():
+        for ip_server_name, server_params in self.ip_apis.items():
             logging.info("Handling server %s with params %s", ip_server_name, server_params)
-            ip_server_data = self.get_ip_server_data(server_params.get("url") ,ip)
+            ip_server_data = await self.get_ip_server_data(server_params.get("url") ,ip)
             total_time += server_params.get("resp_time")
             if ip_server_data is not None:
                 ip_resp[IP_RESP_FIELDS.METRICS.value][ip_server_name] = { IP_RESP_FIELDS.STATUS.value: "success", IP_RESP_FIELDS.TIME.value: server_params.get("resp_time")}
